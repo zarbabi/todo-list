cd25933951dc5970e713a0c058113da48f4a01d2
Zahra Arbabi <z.arbabi4@gmail.com>
2023-11-16 07:40:35 +0330

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  form:FormGroup;
+  tasks:string[]=[]
+   constructor(fb:FormBuilder) {
+   this.form=fb.group({
+     'task':[null,[Validators.required, Validators.minLength(3)]]
+   })
+   }
+removeTask(task:string){
+  const index=  this.tasks.findIndex(x=>x==task)
+  this.tasks.splice(index,1)
+}
+   createTask(){
+     const task=this.form.value.task;
+     if (this.form.invalid){
+       alert('please enter task');
+       return;
+     }
+     this.tasks.push(task);
+     this.form.reset();
+   }
 }
