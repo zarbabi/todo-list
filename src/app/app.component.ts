b7b4136d4b743c3907251aaf9830d76c3c7ae783
Zahra Arbabi <z.arbabi4@gmail.com>
2023-11-16 08:12:12 +0330

    added done list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class AppComponent {
   title = 'todo-list';
   form:FormGroup;
+
   tasks:string[]=[]
+  doneTasks:string[]=[]
    constructor(fb:FormBuilder) {
    this.form=fb.group({
      'task':[null,[Validators.required, Validators.minLength(3)]]
@@ -18,6 +20,10 @@ export class AppComponent {
 removeTask(task:string){
   const index=  this.tasks.findIndex(x=>x==task)
   this.tasks.splice(index,1)
+}
+doneTask(item:string){
+    this.doneTasks.push(item);
+    this.removeTask(item);
 }
    createTask(){
      const task=this.form.value.task;
